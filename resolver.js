95c3a4294baa22157f93da4503c603d68d0a8279
Parihar <Mayank.Parihar@ibsolution.de>
2021-04-14 14:02:17 +0200

    inital graphql with javascript

---
+++ resolver.js
@@ -0,0 +1,31 @@
+const db = require('./db');
+
+const Query = {
+
+    job:(root,{id})=> db.jobs.get(id),
+   // job:(root,args)=>db.jobs.get(args.id),
+    jobs: ()=> db.jobs.list(),
+    company: (root,{id}) => db.companies.get(id)
+}
+
+const Mutation = {
+    createJob: (root, {companyId,title,description}) => {
+       return db.jobs.create({companyId,title,description});
+    }
+};
+
+const Job = {
+
+    company: (job)=> db.companies.get(job.companyId)
+
+}
+
+const Company = {
+
+    jobs: (company) => db.jobs.list()
+        .filter((job)=>job.companyId === company.id)
+
+}
+
+
+module.exports = {Query,Job,Company,Mutation};
